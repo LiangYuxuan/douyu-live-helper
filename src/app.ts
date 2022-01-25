be97900cdb732dfab27c9810f029d4a2ebdce1f9
LiangYuxuan <lyx@lyx.es>
2022-01-25 17:57:11 +0800

    feat: protected call doDonate

--- src/app.ts
+++ src/app.ts
@@ -54,59 +54,75 @@ const loadConfig = () => {
 
     await getGlow(cookies, badges[0].roomID);
 
-    logger.info('获取荧光棒成功');
+    logger.info('获取粉丝荧光棒成功');
 
     const bag = await getBackpack(cookies, 48699);
     let glowCount = bag.list
         .map((value) => value.id === 268 ? value.count : 0)
         .reduce((prev, curr) => prev + curr, 0);
 
-    logger.info('持有荧光棒数量%d', glowCount);
+    logger.info('持有粉丝荧光棒数量%d', glowCount);
 
-    if (config.manual) {
-        for (let index = 0; index < config.roomID.length; ++index) {
-            const roomID = config.roomID[index];
-            const badge = badges.filter((value) => value.roomID === roomID)[0];
-            if (!badge) continue;
+    if (glowCount > 0) {
+        if (config.manual) {
+            for (let index = 0; index < config.roomID.length; ++index) {
+                const roomID = config.roomID[index];
+                const badge = badges.filter((value) => value.roomID === roomID)[0];
+                if (!badge) continue;
 
-            const sendNum =
-                config.sendCount[index] ?? config.sendCount[config.sendCount.length - 1];
+                const sendNum =
+                    config.sendCount[index] ?? config.sendCount[config.sendCount.length - 1];
 
-            logger.debug(
-                'Send Gift 粉丝荧光棒 (268) %d/%d to %s (%s)',
-                sendNum, glowCount, badge.name, badge.medalName,
-            );
+                logger.debug(
+                    'Send Gift 粉丝荧光棒 (268) %d/%d to %s (%s)',
+                    sendNum, glowCount, badge.name, badge.medalName,
+                );
 
-            await doDonate(cookies, roomID, 268, sendNum);
+                try {
+                    await doDonate(cookies, roomID, 268, sendNum);
 
-            glowCount -= sendNum;
+                    glowCount -= sendNum;
 
-            logger.info(
-                '向%s (%s)送出礼物粉丝荧光棒x%d成功: 获得亲密度%d',
-                badge.name, badge.medalName, sendNum, sendNum,
-            );
+                    logger.info(
+                        '向%s (%s)送出礼物粉丝荧光棒x%d成功: 获得亲密度%d',
+                        badge.name, badge.medalName, sendNum, sendNum,
+                    );
+                } catch (error) {
+                    logger.error('向%s (%s)送出礼物粉丝荧光棒x%d失败', badge.name, badge.medalName, sendNum);
+                    logger.error(error);
+                }
+            }
+        } else {
+            const every = Math.floor(glowCount / badges.length);
+            const last = glowCount - every * (badges.length - 1);
+
+            for (let index = 0; index < badges.length; ++index) {
+                const badge = badges[index];
+                const sendNum = index < badges.length - 1 ? every : last;
+
+                if (sendNum > 0) {
+                    logger.debug(
+                        'Send Gift 粉丝荧光棒 (268) %d/%d to %s (%s)',
+                        sendNum, glowCount, badge.name, badge.medalName,
+                    );
+
+                    try {
+                        await doDonate(cookies, badge.roomID, 268, sendNum);
+
+                        glowCount -= sendNum;
+
+                        logger.info(
+                            '向%s (%s)送出礼物粉丝荧光棒x%d成功: 获得亲密度%d',
+                            badge.name, badge.medalName, sendNum, sendNum,
+                        );
+                    } catch (error) {
+                        logger.error('向%s (%s)送出礼物粉丝荧光棒x%d失败', badge.name, badge.medalName, sendNum);
+                        logger.error(error);
+                    }
+                }
+            }
         }
     } else {
-        const every = Math.floor(glowCount / badges.length);
-        const last = glowCount - every * (badges.length - 1);
-
-        for (let index = 0; index < badges.length; ++index) {
-            const badge = badges[index];
-            const sendNum = index < badges.length - 1 ? every : last;
-
-            logger.debug(
-                'Send Gift 粉丝荧光棒 (268) %d/%d to %s (%s)',
-                sendNum, glowCount, badge.name, badge.medalName,
-            );
-
-            await doDonate(cookies, badge.roomID, 268, sendNum);
-
-            glowCount -= sendNum;
-
-            logger.info(
-                '向%s (%s)送出礼物粉丝荧光棒x%d成功: 获得亲密度%d',
-                badge.name, badge.medalName, sendNum, sendNum,
-            );
-        }
+        logger.info('粉丝荧光棒已赠送完毕');
     }
 })();
